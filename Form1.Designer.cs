d9e6fc80604d7b774ea6481402ed157bd22245ec
stasyandr <45917470+stasyandr@users.noreply.github.com>
2020-04-30 23:15:34 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Bonus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(2, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 560);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(657, 36);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(331, 560);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Правила";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(813, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Вывод";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(354, 334);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(251, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(450, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ввод";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(430, 401);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Результат";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(354, 440);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(251, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(409, 468);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 101);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(430, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Указания";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(340, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(265, 231);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 608);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bonus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label6.Text = "Пустое слово: _\nПереход: >\nКаждое правило с новой строки\nПравила писать без пробелов\nЕсли будет выполнено болеше 200 действий программа будет считать схему зациклевшейся" +
+                "\nПрисутвие/отсутвие отступа строки после последнего правила не принципиально";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> d = new Dictionary<string, string>();
+            string s = textBox1.Text;
+            int i = 0;
+            bool flage = false;
+            while (i < s.Length)
+            {
+                try
+                {
+                    int c = s.IndexOf('\n', i);
+                    if (c == -1)
+                        c = s.Length - 1;
+                    int c1 = s.IndexOf(">", i);
+                    string s2 = s.Substring(i, c1 - i);
+                    string s3 = s.Substring(c1 + 1, c - c1);
+                    if (s3.IndexOf('\n') != -1)
+                        s3 = s3.Remove(s3.IndexOf('\n'));
+                    if (s3.Length == 0)
+                        throw new Exception();
+                    if (!d.ContainsKey(s2))
+                        d[s2] = s3;
+                    i = c + 1;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Одно из правил имело неверный формат");
+                    flage = true;
+                    break;
+                }
+            }            
+            /* foreach (var x in d)
+                MessageBox.Show(x.Key + ' ' + x.Value);*/
+            s = textBox3.Text;
+            if (s.Length == 0)
+                s = "_";
+            textBox2.Text = s;
+            if (!flage)
+            {                
+                int k = 0;
+                while (true)
+                {
+                    bool flag = false;
+                    bool flag1 = false;
+                    foreach (var x in d)
+                    {
+                        int c = s.IndexOf(x.Key);
+                        if (c != -1)
+                        {
+                            string r = x.Value;
+                            if (x.Value[0] == '.')
+                            {
+                                flag1 = true;
+                                r = r.Remove(0, 1);
+                            }
+                            s = s.Remove(c, x.Key.Length);
+                            if (r[0] != '_')
+                            {
+                                s = s.Insert(c, r);
+                            }
+                            if (s.Length == 0)
+                                s = "_";
+                            textBox2.Text = textBox2.Text + "->" + s;
+                            flag = true;
+                            break;
+                        }
+                        else
+                        {
+                            if (x.Key.Equals("_"))
+                            {
+                                string r = x.Value;
+                                if (x.Value[0] == '.')
+                                {
+                                    flag1 = true;
+                                    r = r.Remove(0, 1);
+                                }
+                                if (!r.Equals("_"))
+                                    s = s.Insert(0, r);
+                                if (s.Length == 0)
+                                    s = "_";
+                                textBox2.Text = textBox2.Text + "->" + s;
+                                flag = true;
+                                break;
+                            }
+                        }
+                    }
+                    k++;
+                    if (!flag || flag1)
+                        break;
+                    if (k == 200)
+                    {
+                        MessageBox.Show("Произошло зацикливание");
+                        break;
+                    }
+                }
+            }
+            textBox4.Text = s;
+        }
+
+    }
+}
